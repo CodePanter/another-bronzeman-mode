6d2a9ddf061a389234f66fcaa587887e8a389250
CodePanter <codepanter@gmail.com>
2020-04-17 18:17:12 +0200

    Added a runelite test file for the plugin.

---
+++ src/test/java/codepanter/anotherbronzemanmode/AnotherBronzemanModePluginTest.java
@@ -0,0 +1,13 @@
+package codepanter.anotherbronzemanmode;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class AnotherBronzemanModePluginTest
+{
+    public static void main(String[] args) throws Exception
+    {
+        ExternalPluginManager.loadBuiltin(AnotherBronzemanModePlugin.class);
+        RuneLite.main(args);
+    }
+}
